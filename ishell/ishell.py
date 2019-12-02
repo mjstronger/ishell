942d62e0561fe41af829bb3d36441536123f0296
张家铭 <zhangjiaming@inkanshop.com>
2019-12-02 14:15:07 +0800

    feat:增加click工具

---
+++ ishell/ishell.py
@@ -0,0 +1,10 @@
+import click 
+
+
+
+@click.command()
+@click.option('--name', default='', help='Your name')
+def say_hello(name):
+    click.echo("Hello {}!".format(name))
+if __name__ == '__main__':
+    say_hello()
